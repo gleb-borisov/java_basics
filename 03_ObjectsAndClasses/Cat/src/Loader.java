e80807e50f24d8f26c9b789666c069d1c110be4f
Борисов Глеб <glborisov@yandex.ru>
2020-10-09 18:14:29 +0000

    Урок 1, задание 1. Создайте 5–7 кошек, и выведите в консоль их вес.

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,10 +1,31 @@
-
 public class Loader
 {
     public static void main(String[] args)
     {
         Cat cat = new Cat();
 
-        System.out.println(cat.getStatus());
+        Cat murka = new Cat();
+        Cat vaska = new Cat();
+        Cat barsik = new Cat();
+        Cat pirozhok = new Cat();
+        Cat mashka = new Cat();
+        Cat pinochet = new Cat();
+        Cat lapa = new Cat();
+
+        murka.getWeight();
+        vaska.getWeight();
+        barsik.getWeight();
+        pirozhok.getWeight();
+        mashka.getWeight();
+        pinochet.getWeight();
+        lapa.getWeight();
+
+        System.out.println("Вес Мурки = " + murka.getWeight() + " грамм");
+        System.out.println("Вес Васьки = " + vaska.getWeight() + " грамм");
+        System.out.println("Вес Барсика = " + barsik.getWeight() + " грамм");
+        System.out.println("Вес Пирожка = " + pirozhok.getWeight() + " грамм");
+        System.out.println("Вес Машки = " + mashka.getWeight() + " грамм");
+        System.out.println("Вес Пиночета = " + pinochet.getWeight() + " грамм");
+        System.out.println("Вес Лапы = " + lapa.getWeight() + " грамм");
     }
 }
