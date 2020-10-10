3827ee952b60fae3be336e0a7ce95da3cfdb3707
Борисов Глеб <glborisov@yandex.ru>
2020-10-10 05:52:15 +0000

    Урок 1, задание 2. Кормим двух кошек.

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -6,26 +6,13 @@ public class Loader
 
         Cat murka = new Cat();
         Cat vaska = new Cat();
-        Cat barsik = new Cat();
-        Cat pirozhok = new Cat();
-        Cat mashka = new Cat();
-        Cat pinochet = new Cat();
-        Cat lapa = new Cat();
+
+        // Кормим двух кошек
 
         murka.getWeight();
-        vaska.getWeight();
-        barsik.getWeight();
-        pirozhok.getWeight();
-        mashka.getWeight();
-        pinochet.getWeight();
-        lapa.getWeight();
-
         System.out.println("Вес Мурки = " + murka.getWeight() + " грамм");
+
+        vaska.getWeight();
         System.out.println("Вес Васьки = " + vaska.getWeight() + " грамм");
-        System.out.println("Вес Барсика = " + barsik.getWeight() + " грамм");
-        System.out.println("Вес Пирожка = " + pirozhok.getWeight() + " грамм");
-        System.out.println("Вес Машки = " + mashka.getWeight() + " грамм");
-        System.out.println("Вес Пиночета = " + pinochet.getWeight() + " грамм");
-        System.out.println("Вес Лапы = " + lapa.getWeight() + " грамм");
     }
 }
