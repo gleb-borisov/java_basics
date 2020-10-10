d7e832e3a7bab2f5f985c0bb6bd1c3f748ebccb2
Борисов Глеб <glborisov@yandex.ru>
2020-10-10 08:34:31 +0000

    Урок 1, задание 3. Перекармливаем кошку

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -2,17 +2,16 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
-
         Cat murka = new Cat();
-        Cat vaska = new Cat();
-
-        // Кормим двух кошек
 
         murka.getWeight();
-        System.out.println("Вес Мурки = " + murka.getWeight() + " грамм");
 
-        vaska.getWeight();
-        System.out.println("Вес Васьки = " + vaska.getWeight() + " грамм");
+        // Перекармливаем кошку
+
+        while (murka.getWeight() < 9001) {
+            System.out.println("Вес Мурки = " + murka.getWeight() + " грамм");
+            System.out.println(murka.getStatus());
+            murka.feed(1.0);
+        }
     }
 }
