3402c8613e6a7327fc82062d9a61792e7770cf36
Gleb Borisov <glborisov@yandex.ru>
2020-10-21 12:02:56 +0300

    module 3 lesson 6 part 1; made setters and getters for variables in the Car class of the RoadController program

--- 03_ObjectsAndClasses/RoadController/src/core/Car.java
+++ 03_ObjectsAndClasses/RoadController/src/core/Car.java
@@ -7,7 +7,47 @@ public class Car
     public double weight;
     public boolean hasVehicle;
     public boolean isSpecial;
+//======================================================================
+    public void setNumber(String number) { // Создал сеттер для номера
+        this.number = number;
+    }
 
+    public String getNumber() { // Создал геттер для номера
+        return number;
+    }
+//=======================================================================
+    public void setHeight(int height) { // Создал сеттер для высоты
+        this.height = height;
+    }
+
+    public int getHeight() { // Создал геттер для высоты
+        return height;
+    }
+//=======================================================================
+    public void setWeight(double weight) { // Создал сеттер для веса
+        this.weight = weight;
+    }
+
+    public double getWeight() { // Создал геттер для веса
+        return weight;
+    }
+//=======================================================================
+    public void setHasVehicle(boolean hasVehicle) { // Создал сеттер для автомобиля с прицепом
+        this.hasVehicle = hasVehicle;
+    }
+
+    public boolean getHasVehicle() { // Создал геттер для автомобиля с прицепом
+        return hasVehicle;
+    }
+//=======================================================================
+    public void setIsSpecial(boolean isSpecial) { // Создал сеттер для спец транспорта
+        this.isSpecial = isSpecial;
+    }
+
+    public boolean getIsSpecial() { // Создал геттер для спец транспорта
+        return isSpecial;
+    }
+//=======================================================================
     public String toString()
     {
         String special = isSpecial ? "СПЕЦТРАНСПОРТ " : "";
