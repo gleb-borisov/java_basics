746a4fda2d90e28cf8e96814ae54a7ec2da78930
Gleb Borisov <glborisov@yandex.ru>
2020-10-21 23:53:23 +0300

    module 3 lesson 7; made the copy of the Cat object

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -1,45 +1,50 @@
 
-public class Cat<catDead>
+public class Cat
 {
     private double originWeight;
     private double weight;
 
-    public static final double MIN_WEIGHT = 1000; // Константа минимального веса
-    public static final double MAX_WEIGHT = 9000; // Константа максимального веса
-    public static final int CATS_EYES_COUNT = 2; // Константа кол-ва глаз у кошки
+    private double minWeight;
+    private double maxWeight;
 
-    static int count; // Создал статическую переменную count
-
-    private boolean deadCat = false; // Задаем двоичную переменную для определения - жива ли кошка
-
-    private double kitten; // Задал дробную переменную для перегрузки
-
-    private String catColor; // Создал переменную для окраса кошки
-
-    public void setCatColor(String catColor) { // Создал сеттер для окраса
-        this.catColor = catColor;
-    }
-
-    public String getCatColor() { // Создал геттер для окраса
-        return catColor;
-    }
+    private String catName; // Создал текстовую переменную для имени кошки
+    private String catColor; // Создал текстовую переменную для цвета кошки
 
     public Cat()
     {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
-        count++; // Добавил счетчик обьектов
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+
     }
 
-    // Создал перегрузку
-    public Cat (double kitten)
-    {
-        this.kitten = kitten;
+    public Cat (Double weight, String catName, String catColor) { // Конструктор для задания параметров - имя и цвет кошки
+        this.weight = weight;
+        this.catName = catName;
+        this.catColor = catColor;
     }
 
-    // Создал метод по возвращению веса котёнка
-    public double getKittenWeight () {
-        return kitten;
+    public Cat (Cat copy) { // Конструктор для создании копии кошки
+        this.weight = copy.getWeight();
+        this.catName = copy.getCatName();
+        this.catColor = copy.getCatColor();
+    }
+
+     public void setCatName(String catName) { // Создал сеттер для имени кошки
+        this.catName = catName;
+    }
+
+    public String getCatName() { // Создал геттер для имени кошки
+        return catName;
+    }
+
+    public void setCatColor(String catColor) { // Создал сеттер для цвета кошки
+        this.catColor = catColor;
+    }
+
+    public String getCatColor() { // Создал геттер для цвета кошки
+        return catColor;
     }
 
     public void meow()
@@ -50,34 +55,28 @@ public class Cat<catDead>
 
     public void feed(Double amount)
     {
-        if (deadCat() == true) return; // Запрет кушать для мёртвой кошки
         weight = weight + amount;
     }
 
     public void drink(Double amount)
     {
-        if (deadCat() == true) return; // Запрет пить для мёртвой кошки
         weight = weight + amount;
     }
 
-    // Возвратил метод pee с запретом
-    public void pee()
-    {
-        if (deadCat() == true) return; // Запрет для мёртвой кошки ходить в туалет
-        weight = weight - 150.0; // Ёмкость мочевого пузыря кошки = 150 грамм
+    public void setWeight(Double weight) { // Создал сеттер для веса кошки
+        this.weight = weight;
     }
 
-    public Double getWeight()
-    {
+    public Double getWeight() {
         return weight;
     }
 
     public String getStatus()
     {
-        if(weight < MIN_WEIGHT) { // Заменил переменную на константу
+        if(weight < minWeight) {
             return "Dead";
         }
-        else if(weight > MAX_WEIGHT) { // Заменил переменную на константу
+        else if(weight > maxWeight) {
             return "Exploded";
         }
         else if(weight > originWeight) {
@@ -86,18 +85,5 @@ public class Cat<catDead>
         else {
             return "Playing";
         }
-    }
-
-    // Статический метод getCount
-    public static int getCount() {
-        return count;
-    }
-
-    // Метод проверки жива ли кошка
-    public boolean deadCat() {
-        if (getWeight() <= MIN_WEIGHT || getWeight() >= MAX_WEIGHT) { // Заменил переменные на константы
-            return deadCat = true;
-        }
-        else return deadCat = false;
     }
 }
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,23 +1,26 @@
+
 public class Loader
 {
+    public static void main(String[] args)
+    {
+        Cat murka = new Cat(3000.0, "Мурка", "Черный");
 
-    // Создал метод обращения к классу без создания экземпляра класса с заданным параметром 1100 грамм
-    private static Cat getKitten() {
-        return new Cat(1100);
+        System.out.println("Имя Мурки - " + murka.getCatName());
+        System.out.println("Цвет Мурки - " + murka.getCatColor());
+        System.out.println("Вес Мурки - " + murka.getWeight() + " грамм");
+        System.out.println();
+
+        Cat vasya = new Cat(murka);
+
+        System.out.println("Имя Васи - " + vasya.getCatName());
+        System.out.println("Цвет Васи - " + vasya.getCatColor());
+        System.out.println("Вес Васи - " + vasya.getWeight() + " грамм");
+        System.out.println();
+
+        Cat barsik = new Cat(murka);
+
+        System.out.println("Имя Барсика - " + barsik.getCatName());
+        System.out.println("Цвет Барсика - " + barsik.getCatColor());
+        System.out.println("Вес Барсика - " + barsik.getWeight() + " грамм");
     }
-
-    public static void main(String[] args) {
-
-        int i = 1;
-
-        // Создаем три обьекта класса Cat
-        while (i <= 3) {
-            Cat kitten = getKitten(); // Создаём обьект "котёнок" через метод private static Cat getKitten
-            System.out.println("Вес котёнка = " + kitten.getKittenWeight());
-            i = ++i;
-        }
-
-    }
-
-
 }
