40ead1cb24f0065d84ef28b2ae64d81515b74e98
Gleb Borisov <glborisov@yandex.ru>
2020-11-08 18:59:35 +0300

    module 4 task 4.2; max and min value of numbers

---
+++ 04_NumbersStringsAndDates/homework_4.2/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Максимальные и минимальные значения различных типов чисел:");
+        System.out.println();
+        System.out.println("Максимильное значение числа типа byte = " + Byte.MAX_VALUE);
+        System.out.println("Минимальное значение числа типа byte = " + Byte.MIN_VALUE);
+        System.out.println();
+        System.out.println("Максимильное значение числа типа int = " + Integer.MAX_VALUE);
+        System.out.println("Минимальное значение числа типа int = " + Integer.MIN_VALUE);
+        System.out.println();
+        System.out.println("Максимильное значение числа типа short = " + Short.MAX_VALUE);
+        System.out.println("Минимальное значение числа типа short = " + Short.MIN_VALUE);
+        System.out.println();
+        System.out.println("Максимильное значение числа типа long = " + Long.MAX_VALUE);
+        System.out.println("Минимальное значение числа типа long = " + Long.MIN_VALUE);
+        System.out.println();
+        System.out.println("Максимильное значение числа типа float = " + Float.MAX_VALUE);
+        System.out.println("Минимальное значение числа типа float = " + Float.MIN_VALUE);
+        System.out.println();
+        System.out.println("Максимильное значение числа типа double = " + Double.MAX_VALUE);
+        System.out.println("Минимальное значение числа типа double = " + Double.MIN_VALUE);
+    }
+}
