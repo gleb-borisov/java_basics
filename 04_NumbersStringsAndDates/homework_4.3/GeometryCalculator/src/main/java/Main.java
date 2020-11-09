c360a56709d3c4e43d1f1adb99ebe8739a43ec97
Gleb Borisov <glborisov@yandex.ru>
2020-11-09 22:31:47 +0300

    module 4 task 4.3  2ed; additional task, counting through the boxes

--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final int CAR_CAPACITY = 12;
+    private static final int CAR_CAPACITY = 324;
     private static final int CONTAINER_CAPACITY = 27;
 
     public static void main(String[] args) {
@@ -10,41 +10,28 @@ public class Main {
         Scanner inBox = new Scanner(System.in);
         System.out.print("Введите кол-во ящиков - ");
         int box = inBox.nextInt();
-        int container = box / CONTAINER_CAPACITY;
-        int boxRemainder = box % 27;
-        int car = container / CAR_CAPACITY;
-        int containerRemainder = container % 12;
-        int carIn = 0;
-        int containerIn = 0;
-        int boxIn = 0;
+        int container = 0;
+        int car = 0;
 
-        for (int a = 1; a <= car; a++) {
-            System.out.println("Грузовик: " + ++carIn);
-            for (int b = 1; b <= CAR_CAPACITY; b++) {
-                System.out.println("\t Контейнер: " + ++containerIn);
-                for (int c = 1; c <= CONTAINER_CAPACITY; c++) {
-                    System.out.println("\t \t \t Ящик: " + ++boxIn);
-                }
+        for (int a = 1; a <= box; a++) {
+            car = a/CAR_CAPACITY + 1;
+            container = a/CONTAINER_CAPACITY + 1;
+            int carRemainder = a % CAR_CAPACITY;
+            int containerRemainder = a % CONTAINER_CAPACITY;
+
+            if (carRemainder == 0) {
+                System.out.println("Грузовик: " + car);
             }
-        }
 
-        System.out.println("Грузовик: " + ++carIn);
-        for (int d = 1; d <= containerRemainder; d++) {
-            System.out.println("\t Контейнер: " + ++containerIn);
-            for (int e = 1; e <= CONTAINER_CAPACITY; e++) {
-                System.out.println("\t \t \t Ящик: " + ++boxIn);
+            if (containerRemainder == 0) {
+                System.out.println("\t Контейнер: " + container);
             }
+            System.out.println("\t \t \t Ящик:" + a);
         }
-
-        System.out.println("\t Контейнер: " + ++containerIn);
-        for (int f = 1; f <= boxRemainder; f++){
-            System.out.println("\t \t \t Ящик: " + ++boxIn);
-        }
-
         System.out.println();
 
         System.out.println("Необходимо: ");
-        System.out.println("Грузовиков - " + carIn + " шт.");
-        System.out.println("Контейнеров - " + containerIn + " шт.");
+        System.out.println("Грузовиков - " + car + " шт.");
+        System.out.println("Контейнеров - " + container + " шт.");
     }
 }
