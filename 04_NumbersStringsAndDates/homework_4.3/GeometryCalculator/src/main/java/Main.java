a0f9cee1df6b4896a7df93c1da719a5dc37e5396
Gleb Borisov <glborisov@yandex.ru>
2020-11-09 11:16:02 +0300

    module 4 task 4.3; additional task

--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static final int CAR_CAPACITY = 12;
+    private static final int CONTAINER_CAPACITY = 27;
+
     public static void main(String[] args) {
 
+        Scanner inBox = new Scanner(System.in);
+        System.out.print("Введите кол-во ящиков - ");
+        int box = inBox.nextInt();
+        int container = box / CONTAINER_CAPACITY;
+        int boxRemainder = box % 27;
+        int car = container / CAR_CAPACITY;
+        int containerRemainder = container % 12;
+        int carIn = 0;
+        int containerIn = 0;
+        int boxIn = 0;
+
+        for (int a = 1; a <= car; a++) {
+            System.out.println("Грузовик: " + ++carIn);
+            for (int b = 1; b <= CAR_CAPACITY; b++) {
+                System.out.println("\t Контейнер: " + ++containerIn);
+                for (int c = 1; c <= CONTAINER_CAPACITY; c++) {
+                    System.out.println("\t \t \t Ящик: " + ++boxIn);
+                }
+            }
+        }
+
+        System.out.println("Грузовик: " + ++carIn);
+        for (int d = 1; d <= containerRemainder; d++) {
+            System.out.println("\t Контейнер: " + ++containerIn);
+            for (int e = 1; e <= CONTAINER_CAPACITY; e++) {
+                System.out.println("\t \t \t Ящик: " + ++boxIn);
+            }
+        }
+
+        System.out.println("\t Контейнер: " + ++containerIn);
+        for (int f = 1; f <= boxRemainder; f++){
+            System.out.println("\t \t \t Ящик: " + ++boxIn);
+        }
+
+        System.out.println();
+
+        System.out.println("Необходимо: ");
+        System.out.println("Грузовиков - " + carIn + " шт.");
+        System.out.println("Контейнеров - " + containerIn + " шт.");
     }
 }
