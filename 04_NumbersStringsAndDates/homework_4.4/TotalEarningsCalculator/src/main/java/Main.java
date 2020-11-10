8c295270816c91e020227df669a46ec2b81d6252
Gleb Borisov <glborisov@yandex.ru>
2020-11-10 12:21:10 +0300

    module 4 task 4.4 part 2; earnings sum

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,7 +3,19 @@ public class Main {
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+    String textDigit = "";
+    int earning = 0;
+    int sum = 0;
 
+    for (int i = 0; i < text.length(); i++) {
+      Character digitChar = text.charAt(i);
+      if (digitChar.isDigit(digitChar)) {
+        earning = text.indexOf(' ',i);
+        textDigit = text.substring(i,earning);
+        sum = sum + Integer.parseInt(textDigit);
+        i = earning;
+      }
+    }
+    System.out.println("Сумма заработков друзей составила - " + sum + " рублей");
+  }
 }
