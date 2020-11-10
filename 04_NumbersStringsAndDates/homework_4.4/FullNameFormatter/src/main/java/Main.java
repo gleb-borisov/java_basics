0ae11ef653e160bd86bed5aa3472e3cc6f9a8da5
Gleb Borisov <glborisov@yandex.ru>
2020-11-10 23:10:27 +0300

    module 4 task 4.4 part 3; full name formatter

--- 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 
+  private static String nameFamily = "";
+  private static String nameFirst = "";
+  private static String nameSecond = "";
+
   public static void main(String[] args) {
+
+    System.out.println("Введите Фамилию Имя и Отчество через пробел:");
 
     Scanner scanner = new Scanner(System.in);
     while (true) {
@@ -10,9 +16,45 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      for (int i = 0; i < input.length(); i++) {
+        Character digitChar = input.charAt(i);
+        if (digitChar.isDigit(digitChar)) {
+          System.out.println("Введенная строка не является ФИО");
+          return;
+        }
+      }
+
+      input = input.trim();
+      int lenght = input.length();
+
+      for (int a = 0; a < 3; a++) {
+        int substring = input.indexOf(" ");
+        if (substring == -1) {
+          substring = lenght;
+        }
+        if (a == 0) {
+          nameFamily = input.substring(0,substring);
+        }
+        if (a == 1) {
+          nameFirst = input.substring(0,substring);
+        }
+        if (a == 2) {
+          nameSecond = input.substring(0,substring);
+        }
+        input = input.substring(substring,lenght);
+        input = input.trim();
+        lenght = input.length();
+      }
+
+      if (nameFamily == "" || nameFirst == "" || nameSecond == "") {
+        System.out.println("Введенная строка не является ФИО");
+        return;
+      }
+
+      System.out.println("Фамилия: " + nameFamily);
+      System.out.println("Имя: " + nameFirst);
+      System.out.println("Отчество: " + nameSecond);
     }
   }
-
 }
