faf6bcfbb3b04aeb14b596e4f32a42c2081ccd21
Gleb Borisov <glborisov@yandex.ru>
2020-11-11 11:29:45 +0300

    module 4 task 4.4 part 4; masking information

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -1,12 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008> 1234 <5678> 8912", "***");
+        System.out.println(safe);
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        if (text.indexOf("<") == -1 || text.lastIndexOf(">") == -1) {
+            return text;
+        }
+        for (int i = 0; i < text.length(); i++) {
+            if (text.indexOf("<") == -1 || text.indexOf(">") == -1) {
+                break;
+            }
+            int bracketFirst = text.indexOf("<", i);
+            int bracketSecond = text.indexOf(">", i) + 1;
+            i = bracketSecond;
+            String textReplace = text.substring(bracketFirst, bracketSecond);
+            text = text.replaceAll(textReplace, placeholder);
+        }
         return text;
     }
-
 }
