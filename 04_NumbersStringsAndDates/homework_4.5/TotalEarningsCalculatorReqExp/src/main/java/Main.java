eb159607870eb31187f534f8210eca754f33c4ae
Gleb Borisov <glborisov@yandex.ru>
2020-11-11 23:56:30 +0300

    module 4 task 4.5 part 1; total earnings calculator ReqExp

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -2,11 +2,22 @@ public class Main {
 
   public static void main(String[] args) {
 
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    System.out.println("Общий заработок составил - " + calculateSalarySum(text));
+
   }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+
+    String[] earningsString = text.split("\\D+");
+    int[] earning = new int[earningsString.length];
+    int earningSum = 0;
+
+    for (int i = 1; i < earningsString.length; i++) {
+      earning[i] = Integer.parseInt(earningsString[i]);
+      earningSum += earning[i];
+    }
+    return earningSum;
   }
 
 }
