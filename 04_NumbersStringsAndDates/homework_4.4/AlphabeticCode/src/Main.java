e27362ddaeca797be8304b60c3bca804796fdb49
Gleb Borisov <glborisov@yandex.ru>
2020-11-12 09:39:16 +0300

    module 4 task 4.4 part 1  2ed; alphabetic code through a foreach

--- 04_NumbersStringsAndDates/homework_4.4/AlphabeticCode/src/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/AlphabeticCode/src/Main.java
@@ -5,14 +5,14 @@ public class Main {
         char[] alphabetRus = {'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я','А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'};
 
         System.out.println("Латинский шрифт - кодировка:");
-        for (int i = 0; i < alphabetLat.length; i++) {
-            System.out.println(alphabetLat[i] + " - " + (int)alphabetLat[i]);
+        for (char letterLat : alphabetLat) {
+            System.out.println(letterLat + " - " + (int)letterLat);
         }
         System.out.println();
 
         System.out.println("Кириллица - кодировка:");
-        for (int i = 0; i < alphabetRus.length; i++) {
-            System.out.println(alphabetRus[i] + " - " + (int)alphabetRus[i]);
+        for (char letterRus : alphabetRus) {
+            System.out.println(letterRus + " - " + (int)letterRus);
         }
     }
 }
