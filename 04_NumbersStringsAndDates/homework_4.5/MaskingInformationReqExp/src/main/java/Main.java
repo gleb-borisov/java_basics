fd210e94959c4c59fc80ffaa4dedc71b0d50619c
Gleb Borisov <glborisov@yandex.ru>
2020-11-13 18:59:07 +0300

    module 4 task 4.5 aaditional part; masking information ReqExp

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -2,10 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***");
+
+        System.out.println(safe);
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        text = text.replaceAll("(<\\d+>)|(<\\d+.+>)", placeholder);
         return text;
     }
 
