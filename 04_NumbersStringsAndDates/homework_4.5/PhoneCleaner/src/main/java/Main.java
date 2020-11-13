8aa8ec344c8a2ab707ab2f223b0e5f036ffa608c
Gleb Borisov <glborisov@yandex.ru>
2020-11-13 13:02:35 +0300

    module 4 task 4.5 part 4; phone number cleaner

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
 
+  private static String[] phoneNumberArray;
+  private static String number;
+
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
@@ -10,8 +13,34 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+
+      number = "";
+      cleanerPhoneNumber(input);
+
+    if (phoneNumberArray[0].equals("7") || phoneNumberArray[0].equals("8")) {
+        if (number.length() < 11 || number.length() > 11) {
+            System.out.println("Номер телефона введён неверно");
+        } else {
+            System.out.println(number);
+        }
+    } else {
+        System.out.println("Номер телефона введён неверно");
+    }
     }
   }
 
+  public static String cleanerPhoneNumber (String input) {
+      phoneNumberArray = input.split("\\D*");
+      if (phoneNumberArray[0].equals("8")) {
+          phoneNumberArray[0] = "7";
+      }
+      for (String digit : phoneNumberArray) {
+          number = number + digit;
+      }
+      if (number.length() == 10) {
+          number = "7" + number;
+      }
+      phoneNumberArray = number.split("\\D*");
+      return number;
+  }
 }
