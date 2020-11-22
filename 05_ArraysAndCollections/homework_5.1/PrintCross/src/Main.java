bf7d3582e5ac222e02aec5d16c511a2f2d3a7b69
Gleb Borisov <glborisov@yandex.ru>
2020-11-22 21:11:09 +0300

    module 5 task 5.1 additional part  2ed; fixed cross printing

--- 05_ArraysAndCollections/homework_5.1/PrintCross/src/Main.java
+++ 05_ArraysAndCollections/homework_5.1/PrintCross/src/Main.java
@@ -1,20 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
-        String[][] cross = {
-                {"X"," "," "," "," "," ","X"},
-                {" ","X"," "," "," ","X"," "},
-                {" "," ","X"," ","X"," "," "},
-                {" "," "," ","X"," "," "," "},
-                {" "," ","X"," ","X"," "," "},
-                {" ","X"," "," "," ","X"," "},
-                {"X"," "," "," "," "," ","X"},
-        };
+import java.util.Scanner;
 
-        for (int i = 0; i < cross.length; i++) {
-            for (int j = 0; j < cross[i].length; j++) {
-                System.out.print(cross[i][j]);
+public class Main {
+
+    private static String[][] cross;
+
+    public static void main(String[] args) {
+
+        int circle = 0;
+        while (circle == 0) {
+
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите размер стороны квадрата в который будет вписан крест,\nДля выхода из цикла наберите отрицательное число  - ");
+            int arraySize = in.nextInt();
+            if (arraySize < 0) {
+                return;
             }
-            System.out.println();
+            crossCreate(arraySize);
+
+            for (int i = 0; i < cross.length; i++) {
+                for (int j = 0; j < cross[i].length; j++) {
+                    System.out.print(cross[i][j]);
+                }
+                System.out.println();
+            }
         }
+    }
+
+    private static String[][] crossCreate (int arraySize) {
+
+        cross = new String[arraySize][arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            for (int j = 0; j < arraySize; j++) {
+                cross[i][j] = " ";
+                if ((i - j) == 0) {
+                    cross[i][j] = "X";
+                }
+                if (((arraySize - 1 - i) - j) == 0) {
+                    cross[i][j] = "X";
+                }
+            }
+        }
+        return cross;
     }
 }
