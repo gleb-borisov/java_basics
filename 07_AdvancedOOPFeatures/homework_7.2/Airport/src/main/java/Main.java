e2fbc113118697f2b2406f7dc5f5016a519c2d5b
borisov_gleb <glborisov@yandex.ru>
2020-12-31 17:51:07 +0300

    module 7 lesson 2 task 2; Airport

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,18 +1,28 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Airport airport = Airport.getInstance();
+        System.out.println(findPlanesLeavingInTheNextTwoHours(airport));
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        Date date = new Date();
+        Stream <Flight> flightStream = null;
+        for (Terminal terminal : airport.getTerminals()) {
+            flightStream = terminal.getFlights().stream();
+        }
+        return flightStream.filter(x -> x.getDate().getHours() > date.getHours() - 2
+                && x.getDate().getHours() < date.getHours())
+                .collect(Collectors.toList());
     }
-
 }
