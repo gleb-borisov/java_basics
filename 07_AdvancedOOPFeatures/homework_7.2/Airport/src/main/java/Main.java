a57f422f4a7d1d45b42ae8ec07b9583ef1f0d212
borisov_gleb <glborisov@yandex.ru>
2021-01-01 19:22:39 +0300

    module 7 lesson 2 task 2; fixed Airport through flatMap

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,14 +1,12 @@
-import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import com.skillbox.airport.Terminal;
 
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
+
     public static void main(String[] args) {
         Airport airport = Airport.getInstance();
         System.out.println(findPlanesLeavingInTheNextTwoHours(airport));
@@ -17,11 +15,10 @@ public class Main {
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
         Date date = new Date();
-        Stream <Flight> flightStream = null;
-        for (Terminal terminal : airport.getTerminals()) {
-            flightStream = terminal.getFlights().stream();
-        }
-        return flightStream.filter(x -> x.getDate().getHours() > date.getHours() - 2
+        return airport.getTerminals().stream()
+                .map(flightStream -> flightStream.getFlights())
+                .flatMap(flightStream -> flightStream.stream())
+                .filter(x -> x.getDate().getHours() > date.getHours() - 2
                 && x.getDate().getHours() < date.getHours())
                 .collect(Collectors.toList());
     }
