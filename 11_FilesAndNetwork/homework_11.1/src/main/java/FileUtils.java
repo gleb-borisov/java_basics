bd96fd6d35540e08623e42a814c750be4797e4b8
Gleb Borisov <glborisov@yandex.ru>
2021-03-31 19:54:34 +0300

    module 11 lesson 1; reading catalog and files summing up

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,6 +1,30 @@
-public class FileUtils {
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class FileUtils extends Throwable {
+
+    private static long filesSumLength = 0;
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        File folder = new File(path);
+        System.out.println("\nСписок папок и файлов в папке " + path + " -\n");
+        try {
+            recursionModule(folder);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return filesSumLength;
+    }
+
+    public static File recursionModule(File folder) throws FileNotFoundException {
+        for (File file : folder.listFiles()) {
+            System.out.println(file.getName());
+            filesSumLength += file.length();
+            if (file.isDirectory()) {
+                folder = new File(file.getPath());
+                recursionModule(folder);
+            }
+        }
+        return folder;
     }
 }
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите путь до исследуемой папки - ");
+        String path = scanner.nextLine();
+
+        FileUtils fileUtils = new FileUtils();
+        long filesTotalSize = fileUtils.calculateFolderSize(path);
+
+        System.out.println("\nРазмер файлов размещенных на диске D:\n" +
+                "\n\tВ байтах - " + String.format("%,d", filesTotalSize) + " Byte (B)" +
+                "\n\tВ килобайтах - " + String.format("%,.3f", (double)filesTotalSize/1024L) + " KByte (KB)" +
+                "\n\tВ мегабайтах - " + String.format("%,.3f", (double)filesTotalSize/(1024*1024L)) + " MByte (MB)" +
+                "\n\tВ гигабайтах - " + String.format("%,.3f", (double)filesTotalSize/(1024*1024*1024L)) + " GByte (GB)" +
+                "\n\tВ террабайтах - " + String.format("%,.3f", (double)filesTotalSize/(1024*1024*1024*1024L)) + " TByte (TB)" + "\n");
     }
 }
