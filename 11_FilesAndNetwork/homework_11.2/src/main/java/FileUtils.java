e72b32966be39cd209a98c6f64a7399c6eb86a0f
Gleb Borisov <glborisov@yandex.ru>
2021-04-05 12:09:29 +0300

    module 11 lesson 2; folders and files copy

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,47 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class FileUtils {
+
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+        try {
+            Files.walkFileTree(Paths.get(sourceDirectory), new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    System.out.println("Папка - " + dir.toString());
+                    Path path = Paths.get(destinationDirectory).resolve(Paths.get(sourceDirectory).relativize(dir));
+                    if (!Files.exists(path)) {
+                        Files.createDirectory(path);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    System.out.println("Файл - " + file.toString());
+                    Files.copy(file, Paths.get(destinationDirectory).resolve(Paths.get(sourceDirectory).relativize(file)),
+                            StandardCopyOption.REPLACE_EXISTING);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (FileNotFoundException e) {
+            System.out.println("Message of Exception - " + e.getMessage());
+        } catch (IOException e) {
+            System.out.print("Message of Exception - "  + e.getMessage());
+        }
+        System.out.println("Копирование завершено.");
     }
 }
--- 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите путь и имя папки которую хотите скопировать - ");
+        String sourceDirectory = scanner.nextLine();
+        System.out.print("Введите путь и имя папки в которую хотите скопировать содержимое первой папки - ");
+        String destinationDirectory = scanner.nextLine();
+
+        FileUtils fileUtils = new FileUtils();
+        fileUtils.copyFolder(sourceDirectory, destinationDirectory);
     }
 }
