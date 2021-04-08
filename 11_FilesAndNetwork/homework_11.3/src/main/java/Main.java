640f291344511fbf759f09e117f883925fc0d855
Gleb Borisov <glborisov@yandex.ru>
2021-04-09 00:11:46 +0300

    module 11 lesson 3; make sum bank movements

--- 11_FilesAndNetwork/homework_11.3/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.3/src/main/java/Main.java
@@ -1,4 +1,15 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+
+    private static final String BANK_STATEMENT = "src/main/resources/movementList.csv";
+
+    public static void main(String[] args) throws IOException {
+
+        Movements movements = new Movements(BANK_STATEMENT);
+
+        System.out.println("Сумма доходов - " + movements.getIncomeSum() + " руб.");
+        System.out.println("Сумма расходов - " + movements.getExpenseSum() + " руб.");
+        movements.makeMovementList();
     }
 }
--- 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
+++ 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
@@ -1,13 +1,83 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+
 public class Movements {
 
+    private static String pathMovementsCsv;
+    private static List<String> lines;
+    private static HashMap<String, Double> income = new HashMap<>();
+    private static HashMap<String, Double> expense;
+    private static Double sum;
+
     public Movements(String pathMovementsCsv) {
+        this.pathMovementsCsv = pathMovementsCsv;
     }
 
-    public double getExpenseSum() {
-        return 0.0;
+    public static void makeMovementList () throws IOException {
+        incomeList();
+        System.out.println("\nСуммы доходов по организациям:\n");
+        income.forEach((a,b)-> System.out.println(a + " - " + b));
+        expenseList();
+        System.out.println("\nСуммы расходов по организациям:\n");
+        expense.forEach((a,b)-> System.out.println(a + " - " + b));
     }
 
-    public double getIncomeSum() {
-        return 0.0;
+    private static void companyLists () throws IOException {
+        lines = Files.readAllLines(Paths.get(pathMovementsCsv));
+        lines.remove(0);
+        lines.forEach(e -> {String[] fragments = e.split("\\s{4}");
+            income.put(fragments[1], 0.0);});
+        expense = (HashMap<String, Double>) income.clone();
+    }
+
+    private static void incomeList () throws IOException {
+        companyLists();
+        lines.forEach(e -> {
+            String[] fragments1 = e.split("\\s{2,}");
+            String[] fragments2 = e.split("\\s{4}");
+            String[] fragments3 = fragments1[fragments1.length - 1].split(",");
+            if (fragments3.length > 3) {
+                fragments3 = fragments1[fragments1.length - 1].split("\"");
+                if (fragments3.length > 2) {
+                    income.put(fragments2[1], income.get(fragments2[1])
+                            + Double.parseDouble(fragments3[1].replace(',', '.')));
+                }
+            } else {
+                income.put(fragments2[1], income.get(fragments2[1]) + Double.parseDouble(fragments3[1]));
+            }
+        });
+    }
+
+    private static void expenseList () throws IOException {
+        companyLists();
+        lines.forEach(e -> {
+            String[] fragments1 = e.split("\\s{2,}");
+            String[] fragments2 = e.split("\\s{4}");
+            String[] fragments3 = fragments1[fragments1.length - 1].split(",");
+            if (fragments3.length > 3) {
+                fragments3 = fragments1[fragments1.length - 1].split("\"");
+                expense.put(fragments2[1], expense.get(fragments2[1])
+                        + Double.parseDouble(fragments3[1].replace(',', '.')));
+            } else {
+                expense.put(fragments2[1], expense.get(fragments2[1]) + Double.parseDouble(fragments3[2]));
+            }
+        });
+    }
+
+    public double getExpenseSum() throws IOException {
+        expenseList();
+        sum = 0.0;
+        expense.forEach((a, b)-> sum += b);
+        return sum;
+    }
+
+    public double getIncomeSum() throws IOException {
+        incomeList();
+        sum = 0.0;
+        income.forEach((a, b)-> sum += b);
+        return sum;
     }
 }
